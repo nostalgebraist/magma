713ccef0f9ad04d86758fb2dc4eae50ae30d4ea0
nostalgebraist <nostalgebraist@gmail.com>
2022-03-22 19:11:27 -0700

    debug

--- magma/utils.py
+++ magma/utils.py
@@ -213,9 +213,9 @@ def configure_param_groups(model, config):
     print('---')
     for g in all_params:
         # c = Counter(type(p['lr']) for p in g["params"])
-        print(('lr', g['lr']))
+        print(('lr', g.get('lr')))
         # c = Counter(type(p['weight_decay']) for p in g["params"])
-        print(('wd', g['weight_decay']))
+        print(('wd', g.get('weight_decay')))
     print('---')
 
     # merge param dicts with shared lr / wd values
@@ -236,9 +236,9 @@ def configure_param_groups(model, config):
     print('---')
     for g in all_params:
         # c = Counter(type(p['lr']) for p in g["params"])
-        print(('lr', g['lr']))
+        print(('lr', g.get('lr')))
         # c = Counter(type(p['weight_decay']) for p in g["params"])
-        print(('wd', g['weight_decay']))
+        print(('wd', g.get('weight_decay')))
     print('---')
 
     n_params = sum([len(d["params"]) for d in all_params])
