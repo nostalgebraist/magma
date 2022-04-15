1e823332c7aa6f48fd402f213eafecf4ad9e0cbc
nostalgebraist <nostalgebraist@gmail.com>
2022-04-15 12:33:59 -0700

    low system ram

--- magma/magma.py
+++ magma/magma.py
@@ -353,7 +353,7 @@ class Magma(nn.Module):
 
         model = cls(config = config_path)
 
-        sd = torch.load(checkpoint_path, map_location=torch.device("cpu"))
+        sd = torch.load(checkpoint_path, map_location=torch.device(device))
         if "module" in sd.keys():
             sd = sd["module"]
 
